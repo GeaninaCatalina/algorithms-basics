280200fd16d1975f87e4c7eb8708ec5504ef6798
Geanina <masgras.geanina@hotmail.com>
2019-12-26 14:26:19 +0100

    Add Gomoku winning conditions and final alogoritm

--- src/components/gomoku/Gomoku.js
+++ src/components/gomoku/Gomoku.js
@@ -11,60 +11,29 @@ class Gomoku extends React.Component {
     const table = [];
     for (let i = 0; i < 20; i++) {
       table.push([]);
-      for (let j = 2; j < 20; j++) {
+      for (let j = 0; j < 20; j++) {
         table[i].push('');
       }
-    }
+    } 
 
     while (!this.gameShouldEnd(table)) {
+      console.log(table); 
       currentPlayer = !currentPlayer;
       const positions = this.generateMove(table);
       table[positions[0]][positions[1]] = currentPlayer;
-    }
+    } 
 
+    console.log("after"); 
+    console.log(table); 
     const winner = currentPlayer ? input.split(' ')[0] : input.split(' ')[1];
-    result = this.isWinningCondition(table); 
+    result = this.isWinningCondition(table) ? 'The winner is player: ' + winner : 'There is no winner';
     this.props.callBack(result);
-
   }
 
   gameShouldEnd = (array) => {
     return this.allSpacesAreCompleted(array) || this.isWinningCondition(array);
   }
-  
-  isWinningCondition = (array) => { 
-    for (let i = 0; i < 16; i++) {
-      for (let j = 4; j < 16; j++){
-        if (array[i][j] != '') {
-          if (array[i][j] === array[i][j+1] === array[i][j+2] === array[i][j+3] === array[i][j+4]) {
-            return true; 
-          } else if (array[i][j] === array[i+1][j] === array[i+2][j] === array[i+3][j] === array[i+4][j]) {
-            return true;  
-          } else if (array[i][j] === array[i+1][j-1] === array[i+2][j-2] === array[i+3][j-3] === array[i+4][j-4]){
-            return true; 
-          } else if (array[i][j] === array[i+1][j+1] === array[i+2][j+2] === array[i+3][j+3] === array[i+4][j+4]) {
-            return true; 
-          }
-        } 
-      }    
-    }
 
-    return false; 
-  } 
-
-  allSpacesAreCompleted = (array) => {
-    for (let index = 0; index < 19; index++) {
-      for (let j = 0; j < 19; j++) {
-        if (array[index][j] === '') {
-          return false;
-        }
-      }
-    }
-
-    return true;
-  } 
-
-   
   generateMove = (array) => { 
     let found = false; 
 
@@ -77,13 +46,66 @@ class Gomoku extends React.Component {
       }
     }
   }
+  
+  isWinningCondition = (array) => { 
+    for (let i = 0; i < 20; i++) {
+      for (let j = 0; j < 20; j++) {
+        if (array[i][j] !== '') {
+          try {
+          if (array[i][j] === array[i][j+1] && array[i][j+1] === array[i][j+2] && array[i][j+2] === array[i][j+3] && array[i][j+3] === array[i][j+4]) {
+            array[i][j] = 'win'; 
+            array[i][j+1] = 'win'; 
+            array[i][j+2] = 'win'; 
+            array[i][j+3] = 'win'; 
+            array[i][j+4] = 'win'; 
+            return true; 
+          } else if (array[i][j] === array[i+1][j] && array[i+1][j] === array[i+2][j] && array[i+2][j] === array[i+3][j] && array[i+3][j] === array[i+4][j]) { 
+            array[i][j] = 'win'; 
+            array[i+1][j] = 'win'; 
+            array[i+2][j] = 'win'; 
+            array[i+3][j] = 'win'; 
+            array[i+4][j] = 'win'; 
+            return true;  
+          } else if (array[i][j] === array[i+1][j-1] && array[i+1][j-1] === array[i+2][j-2] && array[i+2][j-2] === array[i+3][j-3] && array[i+3][j-3] === array[i+4][j-4]){
+            array[i][j] = 'win'; 
+            array[i+1][j-1] = 'win'; 
+            array[i+2][j-2] = 'win'; 
+            array[i+3][j-3] = 'win';
+            array[i+4][j-4] = 'win';
+            return true; 
+          } else if (array[i][j] === array[i+1][j+1] && array[i+1][j+1] === array[i+2][j+2] && array[i+2][j+2] === array[i+3][j+3] && array[i+3][j+3] === array[i+4][j+4]) {
+            array[i][j] = 'win'; 
+            array[i+1][j+1] = 'win'; 
+            array[i+2][j+2] = 'win';
+            array[i+3][j+3] = 'win';
+            array[i+4][j+4] = 'win';
+            return true; 
+          }
+        } catch (e) {
+          }
+        } 
+      }    
+    }
+    
+    return false; 
+  } 
 
+  allSpacesAreCompleted = (array) => {
+    for (let index = 0; index <= 19; index++) {
+      for (let j = 0; j <= 19; j++) {
+        if (array[index][j] === '') {
+          return false;
+        }
+      }
+    }
 
-
+    return true;
+  } 
+   
   render() {
     return (
         <div>
-          <Button size='mini' color='dark blue' onClick={this.playGameGomoku} className="ui button">Gomoku</Button>
+          <Button size='mini' color='blue' onClick={this.playGameGomoku} className="ui button">Gomoku</Button>
         </div>
     )
   }
