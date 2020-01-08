b08a0685c1571be1b446a676087d5016bc2e592a
Geanina <masgras.geanina@hotmail.com>
2020-01-08 22:10:49 +0100

    gomoku smart phase 3

--- src/components/gomoku/Gomoku.js
+++ src/components/gomoku/Gomoku.js
@@ -104,57 +104,57 @@ class Gomoku extends React.Component {
 
   generateAIMoves = (array) => {
     let table = [];
-    for (let i = 0; i < length.array; i++) {
+    for (let i = 0; i < array.length(); i++) {
       table.push([]);
-      for (let j = 0; j < length.array[0]; j++) {
+      for (let j = 0; j < array[0].length(); j++) {
         table[i].push(0);
       }
     } 
     for (let i = 0; i < 20; i++){
-      for (let j = 0; j < 0; j++){
+      for (let j = 0; j < 20; j++){
         if (array[i][j] === true){ 
           try {
-            table[i][j] === table[i][j] + 1;
+            table[i][j] = table[i][j] + 1;
             //up on row 
-            table[i][j-1] === table[i][j-1] + 1;
-            table[i][j-2] === table[i][j-2] + 1;
-            table[i][j-3] === table[i][j-3] + 1;
-            table[i][j-4] === table[i][j-4] + 1; 
+            table[i][j-1] = table[i][j-1] + 1;
+            table[i][j-2] = table[i][j-2] + 1;
+            table[i][j-3] = table[i][j-3] + 1;
+            table[i][j-4] = table[i][j-4] + 1; 
             //down on row
-            table[i][j+1] === table[i][j+1] + 1;
-            table[i][j+2] === table[i][j+2] + 1;
-            table[i][j+3] === table[i][j+3] + 1;
-            table[i][j+4] === table[i][j+4] + 1; 
+            table[i][j+1] = table[i][j+1] + 1;
+            table[i][j+2] = table[i][j+2] + 1;
+            table[i][j+3] = table[i][j+3] + 1;
+            table[i][j+4] = table[i][j+4] + 1; 
             //right 
-            table[i+1][j] === table[i+1][j] + 1;
-            table[i+2][j] === table[i+2][j] + 1;
-            table[i+3][j] === table[i+3][j] + 1;
-            table[i+4][j] === table[i+4][j] + 1; 
+            table[i+1][j] = table[i+1][j] + 1;
+            table[i+2][j] = table[i+2][j] + 1;
+            table[i+3][j] = table[i+3][j] + 1;
+            table[i+4][j] = table[i+4][j] + 1; 
             //left 
-            table[i-1][j] === table[i-1][j] + 1;
-            table[i-2][j] === table[i-2][j] + 1;
-            table[i-3][j] === table[i-3][j] + 1;
-            table[i-4][j] === table[i-4][j] + 1;  
+            table[i-1][j] = table[i-1][j] + 1;
+            table[i-2][j] = table[i-2][j] + 1;
+            table[i-3][j] = table[i-3][j] + 1;
+            table[i-4][j] = table[i-4][j] + 1;  
             //main diagonal up
-            table[i-1][j-1] === table[i-1][j-1] + 1;
-            table[i-2][j-2] === table[i-2][j-2] + 1;
-            table[i-3][j-3] === table[i-3][j-3] + 1;
-            table[i-4][j-4] === table[i-4][j-4] + 1;  
+            table[i-1][j-1] = table[i-1][j-1] + 1;
+            table[i-2][j-2] = table[i-2][j-2] + 1;
+            table[i-3][j-3] = table[i-3][j-3] + 1;
+            table[i-4][j-4] = table[i-4][j-4] + 1;  
             //main diagonal down 
-            table[i+1][j+1] === table[i+1][j+1] + 1;
-            table[i+2][j+2] === table[i+2][j+2] + 1;
-            table[i+3][j+3] === table[i+3][j+3] + 1;
-            table[i+4][j+4] === table[i+4][j+4] + 1;  
+            table[i+1][j+1] = table[i+1][j+1] + 1;
+            table[i+2][j+2] = table[i+2][j+2] + 1;
+            table[i+3][j+3] = table[i+3][j+3] + 1;
+            table[i+4][j+4] = table[i+4][j+4] + 1;  
             //secundary diagonal up
-            table[i+1][j-1] === table[i+1][j-1] + 1;
-            table[i+2][j-2] === table[i+2][j-2] + 1;
-            table[i+3][j-3] === table[i+3][j-3] + 1;
-            table[i+4][j-4] === table[i+4][j-4] + 1;  
+            table[i+1][j-1] = table[i+1][j-1] + 1;
+            table[i+2][j-2] = table[i+2][j-2] + 1;
+            table[i+3][j-3] = table[i+3][j-3] + 1;
+            table[i+4][j-4] = table[i+4][j-4] + 1;  
             //secundary diagonal down
-            table[i-1][j+1] === table[i-1][j+1] + 1;
-            table[i-2][j+2] === table[i-2][j+2] + 1;
-            table[i-3][j+3] === table[i-3][j+3] + 1;
-            table[i-4][j+4] === table[i-4][j+4] + 1;  
+            table[i-1][j+1] = table[i-1][j+1] + 1;
+            table[i-2][j+2] = table[i-2][j+2] + 1;
+            table[i-3][j+3] = table[i-3][j+3] + 1;
+            table[i-4][j+4] = table[i-4][j+4] + 1;  
           } catch (e) {
             console.log(e); 
           }
@@ -162,6 +162,7 @@ class Gomoku extends React.Component {
         }
       }
     } 
+    console.log(table.join(' '))
     
   }  
 
@@ -169,14 +170,17 @@ class Gomoku extends React.Component {
     let max = 0; 
     let row = 0; 
     let col = 0; 
-    for (let i = 0; i < length.array; i++){
-      for (let j = 0; j < length.array[0]; j++){
+    for (let i = 0; i < array.length(); i++){
+      for (let j = 0; j < array[0].length(); j++){
         if (array[i][j] > max){
           max = array[i][j]; 
           row = i; 
           col = j; 
         }
+      }
+    }   
   }
+   
 
 
    
@@ -186,7 +190,7 @@ class Gomoku extends React.Component {
           <Button size='mini' color='blue' onClick={this.playGameGomoku} className="ui button">Gomoku</Button>
         </div>
     )
-  }
+  } 
 }
 
 export default Gomoku; 
---
+++ src/components/gomoku/testing.js
@@ -0,0 +1,78 @@
+
+  let array1 = [];
+  for (let i = 0; i < 20; i++) {
+    array1.push([]);
+    for (let j = 0; j < 20; j++) {
+      array1[i].push('');
+    }
+  }  
+
+   array1[10][12] = true; 
+   array1[10][13] = true; 
+   array1[11][12] = true; 
+
+
+
+generateAIMoves = (array) => {
+    let table = [];
+    for (let i = 0; i < array.lenght; i++) {
+      table.push([]);
+      for (let j = 0; j < array[0].lenght; j++) {
+        table[i].push(0);
+      }
+    } 
+    for (let i = 0; i < 20; i++){
+      for (let j = 0; j < 20; j++){
+        if (array[i][j] === true){ 
+          try {
+            table[i][j] = table[i][j] + 1;
+            //up on row 
+            table[i][j-1] = table[i][j-1] + 1;
+            table[i][j-2] = table[i][j-2] + 1;
+            table[i][j-3] = table[i][j-3] + 1;
+            table[i][j-4] = table[i][j-4] + 1; 
+            //down on row
+            table[i][j+1] = table[i][j+1] + 1;
+            table[i][j+2] = table[i][j+2] + 1;
+            table[i][j+3] = table[i][j+3] + 1;
+            table[i][j+4] = table[i][j+4] + 1; 
+            //right 
+            table[i+1][j] = table[i+1][j] + 1;
+            table[i+2][j] = table[i+2][j] + 1;
+            table[i+3][j] = table[i+3][j] + 1;
+            table[i+4][j] = table[i+4][j] + 1; 
+            //left 
+            table[i-1][j] = table[i-1][j] + 1;
+            table[i-2][j] = table[i-2][j] + 1;
+            table[i-3][j] = table[i-3][j] + 1;
+            table[i-4][j] = table[i-4][j] + 1;  
+            //main diagonal up
+            table[i-1][j-1] = table[i-1][j-1] + 1;
+            table[i-2][j-2] = table[i-2][j-2] + 1;
+            table[i-3][j-3] = table[i-3][j-3] + 1;
+            table[i-4][j-4] = table[i-4][j-4] + 1;  
+            //main diagonal down 
+            table[i+1][j+1] = table[i+1][j+1] + 1;
+            table[i+2][j+2] = table[i+2][j+2] + 1;
+            table[i+3][j+3] = table[i+3][j+3] + 1;
+            table[i+4][j+4] = table[i+4][j+4] + 1;  
+            //secundary diagonal up
+            table[i+1][j-1] = table[i+1][j-1] + 1;
+            table[i+2][j-2] = table[i+2][j-2] + 1;
+            table[i+3][j-3] = table[i+3][j-3] + 1;
+            table[i+4][j-4] = table[i+4][j-4] + 1;  
+            //secundary diagonal down
+            table[i-1][j+1] = table[i-1][j+1] + 1;
+            table[i-2][j+2] = table[i-2][j+2] + 1;
+            table[i-3][j+3] = table[i-3][j+3] + 1;
+            table[i-4][j+4] = table[i-4][j+4] + 1;  
+          } catch (e) {
+            console.log(e); 
+          }
+         
+        }
+      }
+    } 
+    console.log(table.join(''))
+    
+  } 
